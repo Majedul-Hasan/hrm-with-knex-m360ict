2042c0aaef3260c659a4d3c757d343bf4122cf01
Majedul Hasan <hasanmajedul@gmail.com>
2026-07-07 19:02:02 +0600

    server file

---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import http from 'http';
+
+import app from '@app/index.ts';
+import config from '@shared/config/env.const';
+
+import { connectDatabase, disconnectDatabase } from '@infra/persistence/knex/database';
+import logger from '@infra/logging/logger';
+import redisConnect from '@infra/cache/redis/redis';
+
+const PORT = config.port ?? 5001;
+
+const startServer = async (): Promise<void> => {
+  try {
+    await connectDatabase();
+    redisConnect();
+
+    const httpServer = http.createServer(app);
+
+    httpServer.listen(PORT, () => {
+      logger.info(`🚀 Server running at http://localhost:${PORT}`);
+    });
+
+    const shutdown = async (signal: string): Promise<void> => {
+      logger.info(`${signal} received.`);
+
+      httpServer.close(async () => {
+        await disconnectDatabase();
+
+        logger.info('Application shutdown complete.');
+
+        process.exit(0);
+      });
+    };
+
+    process.on('SIGINT', () => void shutdown('SIGINT'));
+    process.on('SIGTERM', () => void shutdown('SIGTERM'));
+  } catch (error) {
+    logger.error('Application startup failed.', error);
+
+    process.exit(1);
+  }
+};
+
+void startServer();
