32faa0a8fd1b80c901603ce388d639ac40032056
Majedul Hasan <hasanmajedul@gmail.com>
2026-07-07 22:48:06 +0600

    refactor: update .gitignore and clean up unused route imports in index.ts

--- src/infrastructure/http/express/routes/index.ts
+++ src/infrastructure/http/express/routes/index.ts
@@ -1,9 +1,4 @@
-import { AuthRoutes } from '@modules/auth/auth.routes';
-import { RoleRoutes } from '@modules/hr/role/role.routes';
-import { permissionsRoutes } from '@modules/hr/permission/permission.routes';
 import { Router } from 'express';
-import { ProductBrandRoutes } from '@modules/inventory/productBrand/productBrand.routes';
-import { CategoryRoutes } from '@modules/inventory/productCategory/productCategory.routes';
 
 type ModuleRoute = {
   path: string;
@@ -13,26 +8,26 @@ type ModuleRoute = {
 const router = Router();
 
 const moduleRoutes: ModuleRoute[] = [
-  {
-    path: '/auth',
-    route: AuthRoutes,
-  },
-  {
-    path: '/hr/role',
-    route: RoleRoutes,
-  },
-  {
-    path: '/hr/permissions',
-    route: permissionsRoutes,
-  },
-  {
-    path: '/inventory/brand',
-    route: ProductBrandRoutes,
-  },
-  {
-    path: '/inventory/category',
-    route: CategoryRoutes,
-  },
+  // {
+  //   path: '/auth',
+  //   route: AuthRoutes,
+  // },
+  // {
+  //   path: '/hr/role',
+  //   route: RoleRoutes,
+  // },
+  // {
+  //   path: '/hr/permissions',
+  //   route: permissionsRoutes,
+  // },
+  // {
+  //   path: '/inventory/brand',
+  //   route: ProductBrandRoutes,
+  // },
+  // {
+  //   path: '/inventory/category',
+  //   route: CategoryRoutes,
+  // },
 ];
 moduleRoutes.forEach(route => router.use(route.path, route.route));
 
