61621b32c69ab536dc3ddca9e513d72f54ceef5a
Majedul Hasan <hasanmajedul@gmail.com>
2026-07-10 15:15:07 +0600

    feat: refactor BaseRepository methods to standardize parameter types and improve transaction handling

--- src/infrastructure/persistence/repositories/base.repository.ts
+++ src/infrastructure/persistence/repositories/base.repository.ts
@@ -1,30 +1,41 @@
+import { Knex } from 'knex';
 import db from '../knex/knex';
 
 export abstract class BaseRepository<T> {
   constructor(protected readonly table: string) {}
 
-  async findById(id: string) {
-    return db('user as u')
-      .leftJoin('role as r', 'u.roleId', 'r.id')
-      .leftJoin('department as d', 'u.departmentId', 'd.id')
-      .leftJoin('designation as des', 'u.designationId', 'des.id')
-      .select('u.*', 'r.name as roleName', 'd.name as departmentName', 'des.name as designationName')
-      .where('u.id', id)
-      .first();
+  async findById(id: number, trx?: Knex.Transaction): Promise<T | undefined> {
+    const query = trx ?? db;
+    return (await query(this.table).where({ id }).first()) as T | undefined;
   }
 
-  async findByEmail(email: string) {
-    return db('user').where({ email }).first().returning('*');
+  async create(payload: Partial<T>, trx?: Knex.Transaction): Promise<T> {
+    const query = trx ?? db;
+    const [row] = await query(this.table).insert(payload).returning('*');
+    return row as T;
   }
-  async create(payload: Record<string, any>) {
-    const [user] = await db('user').insert(payload).returning('*');
 
-    return this.findById(user.id);
-  }
-  async update(id: string, payload: Record<string, any>) {
-    const [user] = await db('user').where({ id }).update(payload).returning('*');
+  async update(id: number, payload: Partial<T>, trx?: Knex.Transaction): Promise<T> {
+    const query = trx ?? db;
 
-    return user;
+    const [row] = await query(this.table).where({ id }).update(payload).returning('*');
+    return row as T;
   }
-  // delete(id: string) { ... }
+
+  async delete(id: number, trx?: Knex.Transaction): Promise<number> {
+    const query = trx ?? db;
+    return query(this.table).where({ id }).del();
+  }
+
+  async findOne(where: Partial<T>, trx?: Knex.Transaction): Promise<T | undefined> {
+    const query = trx ?? db;
+    return await query(this.table).where(where).first();
+  }
+
+  async exists(where: Partial<T>, trx?: Knex.Transaction): Promise<boolean> {
+    const query = trx ?? db;
+    const row = await query(this.table).where(where).first('id');
+
+    return !!row;
+  }
 }
